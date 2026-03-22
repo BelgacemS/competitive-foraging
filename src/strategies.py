98be79c56e2644a205021a59e8e1e18311b4822f
BelgacemS <belgacem.smaali@gmail.com>
2026-03-23 00:51:33 +0100

    implementation de la stratégie aléatoire et fixe

---
+++ src/strategies.py
@@ -0,0 +1,43 @@
+import numpy as np                                                                                                           
+import random                                                   
+from collections import Counter                                                                                              
+from utils import (generer_allocations, calculer_score,best_response, analyser_allocations)
+
+class Strategie:
+
+    def __init__(self, nom, types_fioles, nb_joueurs=8, allocations=None, meilleure_fixe=None, top_allocs=None):
+        
+        self.nom = nom
+        self.types_fioles = types_fioles
+        self.nb_joueurs = nb_joueurs
+        self.nb_fioles = len(types_fioles)
+        self.allocations = allocations or generer_allocations(nb_joueurs, self.nb_fioles) 
+        self.meilleure_fixe = meilleure_fixe
+        self.top_allocs = top_allocs
+
+    def choisir(self, historique, mon_equipe):
+        raise NotImplementedError
+
+    def reset(self):
+        pass
+
+
+class AleatoireUniforme(Strategie):
+    def __init__(self, types_fioles, allocations=None, meilleure_fixe=None, top_allocs=None):
+        super().__init__("aleatoire", types_fioles, allocations=allocations, meilleure_fixe=meilleure_fixe, top_allocs=top_allocs)
+
+    def choisir(self, historique, mon_equipe):
+        return random.choice(self.allocations)
+
+
+class Fixe(Strategie):
+    def __init__(self, types_fioles, alloc_fixe=None, allocations=None, meilleure_fixe=None, top_allocs=None):
+        super().__init__("tetu", types_fioles, allocations=allocations, meilleure_fixe=meilleure_fixe, top_allocs=top_allocs)
+        
+        self.alloc_fixe = alloc_fixe or self.meilleure_fixe #celle donner par prepare carte
+        
+        if self.alloc_fixe is None:
+            self.alloc_fixe, _ = analyser_allocations(types_fioles, self.allocations)
+
+    def choisir(self, historique, mon_equipe):
+        return self.alloc_fixe
